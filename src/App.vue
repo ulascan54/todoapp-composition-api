9c9fa82cb201830101517ee2b12f8b8219130e6b
ulascan54 <ulascandemirbag@gmail.com>
2021-09-16 22:57:51 +0300

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+<div>
+<div class="bg-gray-700 rounded-md shadow w-[1100px] m-auto mt-10 text-white">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto numquam quidem molestias repudiandae accusamus veritatis voluptates tempore quo aliquid nesciunt facere est beatae mollitia, quos sint temporibus! Porro, consequuntur ipsa!
+</div>
+</div>
+</template>
